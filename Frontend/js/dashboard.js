96ef3bf089d955267b7f2a75607a7a6811783ab6
TonimaDristy <tidristy477@gmail.com>
2026-02-03 22:54:44 +0600

    local storage

--- Frontend/js/dashboard.js
+++ Frontend/js/dashboard.js
@@ -1,6 +1,9 @@
 // frontend>dashboard.js
 
 // TEMP role (later this will come from JWT)
+const role = localStorage.getItem("role");
+
+const welcomeText = document.getElementById("welcomeText");
 
 // If no role is set, redirect to login
 if (!role) {
