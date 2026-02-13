a9865918b6ed823fb06c61dfa6cfb5f10ab03b9e
TonimaDristy <tidristy477@gmail.com>
2026-02-13 22:22:08 +0600

    applyphp

---
+++ Backend/apply.php
@@ -0,0 +1,11 @@
+<?php
+require "config.php";
+
+$name = $_POST['name'];
+$session = $_POST['session'];
+$age = $_POST['age'];
+$phone = $_POST['phone'];
+$course = $_POST['course'];
+$message = $_POST['message'];
+
+
