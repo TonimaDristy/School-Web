0f3d9f6d7b9dd4c49fec3ad1223b160b26f2e321
TonimaDristy <tidristy477@gmail.com>
2026-02-13 22:52:57 +0600

    updated authphp

--- Backend/auth.php
+++ Backend/auth.php
@@ -5,4 +5,10 @@ if (!isset($_SESSION['user_id'])) {
     echo json_encode(["status" => "unauthorized"]);
     exit();
 }
+
+echo json_encode([
+    "status" => "authorized",
+    "name" => $_SESSION['name'],
+    "role" => $_SESSION['role']
+]);
 ?>
