40bf00843f684f29082134ce0d82a9c2d5e16cf5
TonimaDristy <tidristy477@gmail.com>
2026-02-15 19:54:06 +0600

    json_encode

--- Backend/auth.php
+++ Backend/auth.php
@@ -3,10 +3,7 @@ require "config.php";
 header("Content-Type: application/json");
 
 
-if (!isset($_SESSION['user_id'])) {
-    echo json_encode(["status" => "unauthorized"]);
-    exit();
-}
+
 
 echo json_encode([
     "status" => "authorized",
